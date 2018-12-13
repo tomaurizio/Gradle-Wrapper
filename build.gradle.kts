bbae2601d2fd4a1f67bb963310f9709b4b1c3b31
Danilo Pianini <danilo.pianini@unibo.it>
2018-12-13 14:47:56 +0100

    enable Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    jacoco
 }
 
 repositories {
@@ -10,3 +11,6 @@ dependencies {
     implementation("com.google.guava:guava:26.0-jre")
     testImplementation("junit:junit:4.12")
 }
+
+val jacocoTestReport by tasks.named("jacocoTestReport")
+jacocoTestReport.dependsOn(tasks.named("test"))
