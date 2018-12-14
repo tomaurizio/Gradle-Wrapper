6d631edd66469cbd2f582e0e10844b59a13eb9cd
Roberto Casadei <robyonrails@gmail.com>
2018-12-14 11:39:19 +0100

    Test refactoring

---
+++ src/test/java/it/unibo/iot/ProducerConsumerAssertions.java
@@ -0,0 +1,23 @@
+package it.unibo.iot;
+
+import com.google.common.collect.Streams;
+import org.junit.Assert;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProducerConsumerAssertions {
+    public static void assertOrderedConsumptionOnLog(List<String> lines){
+        List<String> producedStuff = lines.stream()
+                .filter(s -> s.contains("Produce"))
+                .map(s -> s.substring(s.lastIndexOf(" ")))
+                .collect(Collectors.toList());
+        List<String> consumedStuff = lines.stream()
+                .filter(s -> s.contains("Consume"))
+                .map(s -> s.substring(s.lastIndexOf(" ")))
+                .collect(Collectors.toList());
+
+        Assert.assertTrue(producedStuff.size()==consumedStuff.size() &&
+                Streams.zip(producedStuff.stream(), consumedStuff.stream(), (a, b) -> a.equals(b)).allMatch(x -> x));
+    }
+}
